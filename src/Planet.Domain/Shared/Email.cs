0fc0df1b37adbe86b571c8ca0a9682cc3f1f1741
Emre Özgenç <emreozgenc@hotmail.com.tr>
2024-03-16 21:34:32 +0300

    user ve email nesneleri

---
+++ src/Planet.Domain/Shared/Email.cs
@@ -0,0 +1,43 @@
+﻿using Planet.Domain.SharedKernel;
+using System.Text.RegularExpressions;
+
+namespace Planet.Domain.Shared
+{
+    public record Email
+    {
+        public string Value { get; init; }
+
+        private Email(string email)
+        {
+            Value = email;
+        }
+
+        private Email() { }
+
+        public static Email Create(string email)
+        {
+            if(!IsValidEmail(email))
+            {
+                throw new DomainException("Email.NotValid", "Geçerli bir e-posta adresi giriniz!");
+            }
+
+            if(IsInRange(email))
+            {
+                throw new DomainException("Email.InvalidRange", "E-posta adresi 100 karakterden uzun olamaz!");
+            }
+
+            return new Email(email);
+        }
+
+        private static bool IsInRange(string email)
+        {
+            return email.Length <= 100;
+        }
+
+        private static bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+        }
+
+    }
+}
---
+++ src/Planet.Domain/Users/User.cs
@@ -0,0 +1,22 @@
+﻿using Planet.Domain.Shared;
+using Planet.Domain.SharedKernel;
+
+namespace Planet.Domain.Users
+{
+    public class User : Entity, IAggregateRoot
+    {
+        public Email Email { get; private set; }
+        public string Password { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+        public bool IsActive { get; private set; }
+
+        private User() : base(Guid.Empty) { }
+
+        public void ChangeActivity(bool activity)
+        {
+            IsActive = activity;
+        } 
+    }
+}
