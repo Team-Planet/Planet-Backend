61615559ad27d370b3ce3fdd4bb6d1350fa9ef46
Emre Özgenç <emreozgenc@hotmail.com.tr>
2024-03-20 21:33:02 +0300

    feat : seeding extensions infrastructure

---
+++ src/Planet.Persistence/Seeding/SeedingExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Planet.Persistence.Contexts;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+
+namespace Planet.Persistence.Seeding
+{
+    public static class SeedingExtensions
+    {
+        public static async Task SeedAsync(this IApplicationBuilder builder, IConfiguration configuration)
+        {
+            using var serviceScope = builder.ApplicationServices.CreateScope();
+            using var context = serviceScope.ServiceProvider.GetRequiredService<PlanetContext>();
+
+
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
