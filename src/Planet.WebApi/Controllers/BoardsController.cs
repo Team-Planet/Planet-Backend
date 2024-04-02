5bca920b962f6aedbb3c996a53c1cbb2f6f0f510
Emre Özgenç <emreozgenc@hotmail.com.tr>
2024-04-02 22:37:46 +0300

    feat : boardscontroller

---
+++ src/Planet.WebApi/Controllers/BoardsController.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Planet.WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BoardsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BoardsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return await Task.FromResult(Ok(""));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit()
+        {
+            return await Task.FromResult(Ok(""));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMember()
+        {
+            return await Task.FromResult(Ok(""));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveMember()
+        {
+            return await Task.FromResult(Ok(""));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddList()
+        {
+            return await Task.FromResult(Ok(""));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveList()
+        {
+            return await Task.FromResult(Ok(""));
+        }
+    }
+}
