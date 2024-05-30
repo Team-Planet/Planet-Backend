d05dd610422f80bfccaaab040a4d81320334b91b
Mehmet Burkay Kıraç <kiracmehmetburkay@gmail.com>
2024-05-30 04:31:00 +0300

    bugfix: CardStartDate and CardEndDate changed as nullable

--- src/Planet.Domain/Cards/CardDates.cs
+++ src/Planet.Domain/Cards/CardDates.cs
@@ -2,22 +2,22 @@
 {
     public record CardDates
     {
-        public DateTime StartDate { get; init; }
-        public DateTime EndDate { get; init; }
+        public DateTime? StartDate { get; init; }
+        public DateTime? EndDate { get; init; }
 
         private CardDates() { }
 
         private CardDates(
-            DateTime startDate,
-            DateTime endDate)
+            DateTime? startDate,
+            DateTime? endDate)
         {
             StartDate = startDate;
             EndDate = endDate;
         }
 
         public static CardDates Create(
-            DateTime startDate,
-            DateTime endDate)
+            DateTime? startDate,
+            DateTime? endDate)
         {
             return new CardDates(startDate, endDate);
         }
